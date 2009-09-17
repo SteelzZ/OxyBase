e880cef8942384383e15b8140946edd6f56b63ab
to.bartkus <to.bartkus@edb9dc4c-75fc-11de-bf7b-bff11111869b>
2009-09-17 13:12:23 +0000

    Deleted Oxy_Execute

--- trunk/application/library/Oxy/Event/Exception.php
+++ trunk/application/library/Oxy/Event/Exception.php
@@ -2,8 +2,8 @@
 /**
 * Oxy_Event_Exception
 *
-* @category
-* @package
+* @category Oxy
+* @package Oxy_Event
 * @author Tomas Bartkus <to.bartkus@gmail.com>
 **/
 class Oxy_Event_Exception extends Oxy_Exception
